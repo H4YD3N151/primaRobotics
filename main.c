556203f0c3750f7b007b75d5e2afa66f4b667295
H4YD3N151 <32693932+H4YD3N151@users.noreply.github.com>
2017-10-16 14:38:44 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 //Declare any variables shared between functions here
+float myPosA[3];
+float myPosB[3];
+float attitudeA[3];
+float attitudeB[3];
+int counter;
 
 void init(){
+    
+    myPosA[0] = 0.6;
+    myPosA[1] = -0.6;
+    myPosA[2] = 0;
+    
+    myPosB[0] = 0;
+    myPosB[1] = 0;
+    myPosB[2] = 0;
+    
+    attitudeA[0] = -0.6;
+    attitudeA[1] = 0.6;
+    attitudeA[2] = 0;
+    
+    attitudeB[0] = 0;
+    attitudeB[1] = 0;
+    attitudeB[2] = 0;    
+    
+    counter = 0;
+    
 	//This function is called once when your code is first loaded.
 
 	//IMPORTANT: make sure to set any variables that need an initial value.
@@ -9,4 +33,23 @@ void init(){
 
 void loop(){
 	//This function is called once per second.  Use it to control the satellite.
+
+    api.setPositionTarget(myPosA);
+    if(counter == 40){
+        game.startDrill();
+        api.setAttitudeTarget(attitudeA);
+        //when the timer hits 40 sec the drill starts and the shpere spins
+    }
+    if(counter > 62){
+        api.setPositionTarget(myPosB);
+        game.pickupSample();
+        game.stopDrill();
+        api.setAttitudeTarget(attitudeB);
+        //at 62 sec the drill stops, the sample is picked up, and the sphere moves to base
+    }
+    if(counter == 100){
+        game.dropSample(0);
+        //at 100 sec the sphere drops the sample at base
+    }
+    counter++;
 }
